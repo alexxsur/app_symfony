9b87b2cb8dd066494556ab032c599021549607e8
Alex <alexxsur@gmail.com>
2023-08-22 18:59:33 -0400

    Create HelloController and method hello

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class HelloController {
+
+    function hello(){
+        return new Response('Hello !');
+    }
+}
