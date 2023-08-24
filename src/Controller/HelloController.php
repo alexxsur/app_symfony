6c967fd4cccd8a4b7e1ac273e6bd9bd6b589fa68
Alex <alexxsur@gmail.com>
2023-08-24 17:37:28 -0400

    Add annotation route

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -5,9 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController {
 
+    /**
+    * @Route("hello")
+    */
     function hello(Request $request){
         $params = $request->query->all();
         $string = "Les paramètres sont: </br>";
