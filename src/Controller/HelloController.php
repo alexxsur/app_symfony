c7e34d1244708f6ed612aea7b34e73823aa656b8
Alex <alexxsur@gmail.com>
2023-08-24 11:31:41 -0400

    Request and Response objects

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,15 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class HelloController extends AbstractController {
 
-    function hello(){
-        $title = "utilisateurs";
-        $users = ["Mickey", "Leo", "Donnie", "Raph"];
-
-        return $this->render("hello.html.twig",
-            ['title' => $title, 'array' => $users]);
+    function hello(Request $request){
+        $params = $request->query->all();
+        $string = "Les paramètres sont: </br>";
+        foreach ($params as $key => $value) {
+            $string = $string . '-' . $key . ':' . $value . '</br>';
+        }
+        return new Response($string);
     }
 }
