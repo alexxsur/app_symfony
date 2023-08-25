ad17c030e263f979fc56a981b311b4b9c8c34b76
Alex <alexxsur@gmail.com>
2023-08-25 11:11:12 -0400

    Create form

---
+++ src/Controller/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+
+class UserController extends AbstractController {
+
+    /**
+    * @Route("/user")
+    */
+    function createUserFor(){
+        $user = new User();
+        $form = $this->createFormBuilder($user)
+            ->add('name')
+            ->add('email')
+            ->getForm();
+        return $this->render('form.html.twig', ['userForm' => $form->createView()]);
+    }
+}
