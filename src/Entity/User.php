24c26aec9cede2d53b8951ae1dfa3c8704974f44
Alex <alexxsur@gmail.com>
2023-08-25 07:23:00 -0400

    Create form entity

---
+++ src/Entity/User.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Entity;
+
+class User{
+
+    protected $name;
+    protected $email;
+
+    /**
+     * Get the value of name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get the value of email
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+}
