fc395c1d1e9d19d5d0a8565616b75abd711ce70a
Alex <alexxsur@gmail.com>
2023-08-26 06:30:42 -0400

    Create service

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,19 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Psr\Log\LoggerInterface;
+use App\Service\Greeter;
+use Symfony\Component\HttpFoundation\Response;
 
 class HelloController extends AbstractController {
 
     /**
     * @Route("hello")
     */
-    function hello(LoggerInterface $logger){
-        $logger->alert('logger!');
-        return $this->render('hello.html.twig');
+    function hello(Greeter $greeter){
+        $message = $greeter->greet();
+        return new Response($message);
     }
 
 }
---
+++ src/Service/Greeter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Service;
+
+class Greeter{
+
+    public function greet(){
+        return "Hello!";
+    }
+}
