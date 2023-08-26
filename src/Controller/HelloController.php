0338858321cd4b352e26a3cacc6916a732f0341f
Alex <alexxsur@gmail.com>
2023-08-25 21:40:36 -0400

    Add symfony logger service

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -6,17 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
 
 class HelloController extends AbstractController {
 
     /**
-    * @Route({
-    *      "fr": "/bonjour",
-    *      "en": "/hello"})
+    * @Route("hello")
     */
-    function hello(Request $request){
-        $locale = $request->getLocale();
-        return new Response('Hello, locale :' . $locale);
+    function hello(LoggerInterface $logger){
+        $logger->alert('logger!');
+        return $this->render('hello.html.twig');
     }
 
 }
